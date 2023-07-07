128cb99f186f1caaffb29989048cda41a02a7308
Shubham Thakur <sbmthakur@gmail.com>
2023-07-07 13:12:25 -0400

    Add a test

--- test.js
+++ test.js
@@ -5,6 +5,34 @@ describe('Test Points', () => {
   it('should return points', () => {
 
     let data = {
+      "retailer": "TargetD",
+      "purchaseDate": "2022-01-01",
+      "purchaseTime": "13:01",
+      "items": [
+        {
+          "shortDescription": "Mountain Dew 12PK",
+          "price": "6.49"
+        },{
+          "shortDescription": "Emils Cheese Pizza",
+          "price": "12.25"
+        },{
+          "shortDescription": "Knorr Creamy Chicken",
+          "price": "1.26"
+        },{
+          "shortDescription": "Doritos Nacho Cheese",
+          "price": "3.35"
+        },{
+          "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+          "price": "12.00"
+        }
+      ],
+      "total": "35.00"
+    }
+
+    let points = calculatePoints(data)
+    assert(points === 104)
+
+    data = {
       "retailer": "Target",
       "purchaseDate": "2022-01-01",
       "purchaseTime": "13:01",
@@ -29,7 +57,7 @@ describe('Test Points', () => {
       "total": "35.35"
     }
 
-    let points = calculatePoints(data)
+    points = calculatePoints(data)
     assert(points === 28)
 
     data = {
